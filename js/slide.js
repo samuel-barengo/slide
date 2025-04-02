aaef3f9b9e4e89118ca4664d59057c5c694e1847
Samuel barengo <samuk-09@hotmail.com>
2025-04-02 19:56:04 -0300

    calculo de distancias quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -2,23 +2,36 @@ export default class Slide {
     constructor(wrapper, slide) {
         this.wrapper = document.querySelector(wrapper);
         this.slide = document.querySelector(slide);
+        this.dist = { finalPosition: 0, startX: 0, movement: 0 }
     }
 
+    moveSlide(distX) {
+        this.dist.movePosition = distX;
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+    }
+    updatePosition(clientX) {
+        (this.dist.movement = this.dist.startX - clientX) * 1.6;
+        return this.dist.finalPosition - this.dist.movement;
+    }
     onStart(event) {
         event.preventDefault();
+        this.dist.startX = event.clientX;
         this.wrapper.addEventListener('mousemove', this.onMove);
     }
 
     onMove(event) {
+        const finalPosition = this.updatePosition(event.clientX);
+        this.moveSlide(finalPosition)
     }
 
     onEnd(event) {
+        this.dist.finalPosition = this.dist.movePosition
         this.wrapper.removeEventListener('mousemove', this.onMove);
     }
 
     addEventSlide() {
         this.wrapper.addEventListener('mousedown', this.onStart);
-        this.wrapper. addEventListener('mouseup', this.onEnd);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
     }
 
     bindEvent() {
@@ -33,3 +46,6 @@ export default class Slide {
         return this;
     }
 }
+
+// As propriedades clientX e clientY em JavaScript 
+// retornam as coordenadas do mouse em relação à janela visível do navegador (viewport), sem contar rolagens da página.
